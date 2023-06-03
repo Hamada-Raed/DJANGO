4a656ce8b5916609ae84ab9eb6ba755a4a42af6e
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-06-03 11:36:09 +0300

    The first assignment in DJANGO

---
+++ First_project_in_Django/myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render , HttpResponse ,redirect
+
+def index(request):
+    return redirect("/blogs") 
+
+def root(request): 
+    return HttpResponse ('placeholder to later display a list of all blogs" with a method named "index"')
+
+def new(request): 
+    return HttpResponse ('display the string "placeholder to display a new form to create a new blog" with a method named "new"')
+
+def create(request): 
+    return redirect ('/')
+
+def show(request, number): 
+    return HttpResponse (f'placeholder to display blog number {number}')  
+
+def edit(request, number): 
+    return HttpResponse (f"placeholder to edit blog {number}") 
+
+def destroy(request,number):
+    return redirect ('/')
+
+
