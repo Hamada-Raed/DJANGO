f3b9b863acfbfb48288739c9d446f720ec07ae6e
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-06-08 00:08:18 +0300

    ninja gold

---
+++ ninja_gold/myapp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+import random 
+def index(request):
+    if 'gold' not in request.session:
+        request.session['gold'] = 0 
+
+    if 'count' not in request.session: 
+        request.session['count'] = 0 
+
+    if 'num' not in request.session: 
+        request.session['num']= request.POST.get('number', False)
+
+    if 'text' not in request.session: 
+        request.session['text']=''
+    content = {
+        'gold_money' : request.session['gold'], 
+        'counter' : request.session['count'], 
+        'massage' : request.session['text'],
+    }
+    return render (request, 'index.html',content) 
+
+def money(request):
+    method = request.POST['money'] 
+    if method == 'farm'or 'cave' or 'house': 
+        amount = random.randint(10,20)
+        request.session['gold']+=amount 
+
+    elif method == 'quest':  
+        amount = random.randint(0,50) 
+        request.session['gold']+=amount 
+
+    elif request.session['gold'] >= request.session['num'] :
+        request.session['text']='YOU WIN'
+
+    request.session['count']+=1
+    response = redirect('/')
+    return response
+
+def delet(request): 
+    del request.session['gold']
+    response = redirect('/')
+    return response
