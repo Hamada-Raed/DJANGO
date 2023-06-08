82fccebbae8611fae3caa44dbbe03e20b62f41b6
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-06-08 18:18:07 +0300

    modifiy

--- ninja_gold/myapp/views.py
+++ ninja_gold/myapp/views.py
@@ -8,10 +8,12 @@ def index(request):
         request.session['count'] = 0 
 
     if 'num' not in request.session: 
-        request.session['num']= request.POST.get('number', False)
+        request.session['num']= request.POST['number']
 
     if 'text' not in request.session: 
         request.session['text']=''
+        
+    request.session['text']=''
     content = {
         'gold_money' : request.session['gold'], 
         'counter' : request.session['count'], 
@@ -24,11 +26,12 @@ def money(request):
     if method == 'farm'or 'cave' or 'house': 
         amount = random.randint(10,20)
         request.session['gold']+=amount 
+        
 
     elif method == 'quest':  
         amount = random.randint(0,50) 
         request.session['gold']+=amount 
-
+    
     elif request.session['gold'] >= request.session['num'] :
         request.session['text']='YOU WIN'
 
