3ab5874e9ae963642e11400ebb101fca70ca0689
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-06-08 20:17:38 +0300

    First ORM

--- my_orm/my_orm/urls.py
+++ my_orm/my_orm/urls.py
@@ -1,4 +1,4 @@
-"""my_first_orm URL Configuration
+"""my_orm URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
