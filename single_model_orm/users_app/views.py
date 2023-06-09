f1f97f419ef8b31f5bb1081b5a961da5d1a82288
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-06-09 13:14:54 +0300

    db with templates

--- single_model_orm/users_app/views.py
+++ single_model_orm/users_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import users
 
-# Create your views here.
+def index(request):
+    context = {
+    	"all_the_users": users.objects.all()
+    }
+    return render(request, "index.html", context)
