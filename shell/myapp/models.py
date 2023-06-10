f2f847913542307915784ba056022687ee184c0a
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-06-10 18:57:32 +0300

    modified shell project and add another one, about one to many relationship

--- shell/myapp/models.py
+++ shell/myapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=45)
+    pages = models.TextField()
     description = models.TextField()
     release_date = models.DateField()
     duration = models.IntegerField()
