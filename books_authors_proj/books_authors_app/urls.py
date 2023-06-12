f5c31e058e4cfdec987640c82601515ab42da8b7
Hamada <HamadaRaed1999@sharafcutersair.mynet>
2023-06-12 23:19:08 +0300

    Commit

--- books_authors_proj/books_authors_app/urls.py
+++ books_authors_proj/books_authors_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path ('add_book', views.add_book),
+    path ('view_book' , views.view_book)
 ]
--- books_authors_proj/books_authors_app/views.py
+++ books_authors_proj/books_authors_app/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from .models import Book, Author
+def index(request):
+   
+    content = {
+        'all_books' : Book.objects.all(),
 
-def index(request): 
-    return render (request , 'index.html')
+    }
+    return render (request , 'index.html',content)
+
+def add_book(request): 
+    new_book = Book.objects.create(title = request.POST['name'], desc = request.POST['description'])
+    content = {
+        'all_books' : Book.objects.all(), 
+        'all_books' : new_book
+    }
+    return redirect ('/')
+
+def view_book(request):
+    return render (request , 'view_book.html')
