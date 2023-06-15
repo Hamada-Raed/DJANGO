f6a4855cc0aebffb92ea62903fb31b67deb3315f
Hamada <HamadaRaed1999@SHARAFCOMPUTERs-MacBook-Air.local>
2023-06-15 10:43:53 +0300

    one to many in dojo and ninja

---
+++ dojo_ninja/dojo_ninja_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from .models import Ninja, Dojo
+
+
+def index(request):
+    context = {
+        "dojo_objects": Dojo.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
+def add_dojo(request):
+    Dojo.objects.create(
+        name=request.POST['name'],
+        city=request.POST['city'],
+        state=request.POST['state']
+    )
+    return redirect('/')
+
+def adding_n(request):
+    Ninja.objects.create(
+        first_name=request.POST['first'],
+        last_name=request.POST['last'],
+        dojo=Dojo.objects.get(id=request.POST['dojo']),
+    )
+
+    return redirect('/')
+
+
+def deleteDojo(request):
+    dell = Dojo.objects.get(id=request.POST['delete']).delete()
+
+    return redirect('/')
