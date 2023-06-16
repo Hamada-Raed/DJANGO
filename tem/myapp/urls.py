3bbf14fb73e63b4cb34c3e53a2b2f1ccfd36f0c8
Hamada-Raed <stu.no1663@gmail.com>
2023-06-16 15:17:00 +0300

    commit

--- tem/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path ,include 
-from . import views
-urlpatterns = [
-    path('', views.index),
-    path('<name>/', views.hello), 
-    path('<name>/<int:age>', views.hello),
-    path('<name>/<int:age>/<int:id>', views.hello)  
-]
--- tvshow/tvapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class show(models.Model):
-    name = models.CharField(max_length=255)
-    network = models.CharField(max_length=255)
-    desc = models.TextField()
-    release_date = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
