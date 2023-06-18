c96030dfc95c13ccfc42d4237b24d530935142c4
Hamada-Raed <stu.no1663@gmail.com>
2023-06-18 13:22:21 +0300

    First commit in Vaildation

---
+++ vaildation/vailapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+
+
+
---
+++ vaildation/vaildation/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('vailapp.urls')),
+]
